6dd15d4a2dc64e775e9a1290bff5f4d32852767f
JY <rd-sw13@visionsecurity.com.tw>
2021-07-13 08:31:29 +0800

    Initial Commit

---
+++ WeChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeChat
+//
+//  Created by JianYu on 2021/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
